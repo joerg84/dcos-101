d3fae917c534b7aed3e3645d294e96dbaaa2c713
Joerg Schad <joerg@mesosphere.io>
2016-09-19 15:51:51 +0200

    Added initial Go webapp example

---
+++ app2/app.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Welcome to DC/OS 101! \n")
+    fmt.Fprintf(w, "Running on host '"+  os.Getenv("HOST") + "' and port '" + os.Getenv("PORT0") + "'")
+}
+
+func main() {
+    fmt.Println("Starting DC/OS-101 App ")
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":" + os.Getenv("PORT0") , nil)
+}
