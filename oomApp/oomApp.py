549bc126720c3c533d9afa037587a5eaad85c5c9
Joerg Schad <joerg@mesosphere.io>
2016-09-28 11:28:44 +0200

    Update oomApp.py

--- oomApp/oomApp.py
+++ oomApp/oomApp.py
@@ -3,10 +3,10 @@ import time
 
 storage = {};
 for i in range(1, 100):
-  big_str = ' ' * 512000000
+  big_str = ' ' * 1000000
   storage[i] = some_str
   time.sleep(0.1)
-  print "Eat 512 MB"
+  print "Eat 1 MB"
 
 print "Satisfied!"
 time.sleep(10)
