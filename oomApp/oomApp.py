a717dbf8b100e24123ae245cd7c3b7f174377e87
Joerg Schad <joerg@mesosphere.io>
2016-09-28 11:13:15 +0200

    Added ooM App

---
+++ oomApp/oomApp.py
@@ -0,0 +1,12 @@
+
+import time
+
+storage = {};
+for i in range(1, 100):
+  big_str = ' ' * 512000000
+  storage[i] = some_str
+  time.sleep(0.1)
+  print "Eat 512 MB"
+
+print "Satisfied!"
+time.sleep(10)
