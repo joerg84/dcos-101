86ac8b86b3333e9d5ce64ad0408aac515646693b
osboxes.org <joerg@mesosphere.io>
2016-10-05 14:22:20 +0100

    Updated app2.

--- app2/app2.go
+++ app2/app2.go
@@ -4,11 +4,26 @@ import (
     "fmt"
     "net/http"
     "os"
+
+    "gopkg.in/redis.v4"
 )
+
+func getKeys(w http.ResponseWriter) {
+    // Connect to redis.
+    client := redis.NewClient(&redis.Options{
+        Addr:     "redis.marathon.l4lb.thisdcos.directory:6379",
+        Password: "", // no password set
+        DB:       0,  // use default DB
+    })
+
+    pong, _ := client.Ping().Result()
+    fmt.Fprintf(w, "Pong " + pong +"\n")
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Welcome to DC/OS 101! \n")
-    fmt.Fprintf(w, "Running on host '"+  os.Getenv("HOST") + "' and port '" + os.Getenv("PORT0") + "'")
+    fmt.Fprintf(w, "Running on node '"+  os.Getenv("HOST") + "' and port '" + os.Getenv("PORT0") + "' \n")
+    getKeys(w)
 }
 
 func main() {
